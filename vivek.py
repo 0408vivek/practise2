dcaceb1fc067572016ef364fc5b2ad3b905aa2bb
vivek sharma <vivek9893@gmail.com>
2023-07-25 11:47:27 +0530

    merge

--- vivek.py
+++ vivek.py
@@ -1,7 +1,8 @@
-def Car:
-   def __init__(self,name,brand):
-      self.name=name
-      self.brand=brand
-car1=Car("maruti","alto")
+class Car:
+    def __init__(self,name,brand):
+        self.name=name
+        self.brand=brand
+car1=Car("alto","maruti")
 car1.name()
-    
+
+
