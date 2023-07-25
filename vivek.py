abbdd9860b7ad9a18d54895cb20b1c07572b440e
vivek sharma <vivek9893@gmail.com>
2023-07-25 12:24:08 +0530

    chenge

--- vivek.py
+++ vivek.py
@@ -2,9 +2,10 @@ class Car:
     def __init__(self,name,brand):
         self.name=name
         self.brand=brand
+        
     def show(self):
         print(f"this is my {self.name} and this is my brand {self.brand}")
-car1=Car(alto,maruti)
+car1=Car("alto","maruti")
 print(car1.name())
 print(car1.show())
 
